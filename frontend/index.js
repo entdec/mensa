cd30d4939cf57531d51e179e3a41786ac96483a0
Tom de Grunt <tom@degrunt.nl>
2024-01-24 21:17:26 +0100

    frontend + css

---
+++ frontend/index.js
@@ -0,0 +1,32 @@
+export class Mensa {
+  static start(application, configuration = {}) {
+    if (!application) {
+      application = Application.start()
+    }
+
+    this.application = application
+    this.application.mensa = {
+      configuration: configuration,
+    }
+
+    console.log("Mensa")
+
+    Mensa.setupControllers()
+  }
+
+  static setupControllers() {
+    const componentControllers = require.context("../app/components/", true, /component_controller\.js$/)
+
+    componentControllers
+      .keys()
+      .map((key) => {
+        // Take the last part (before component_controller) of the path as the name
+        const [_, name] = /([^/]+)\/component_controller\.js$/.exec(key)
+        return [name, componentControllers(key).default]
+      })
+      .forEach(([name, controller]) => {
+        let identifier = `mensa-${name.replace(/_/g, "-")}`
+        this.application.register(identifier, controller)
+      })
+  }
+}
