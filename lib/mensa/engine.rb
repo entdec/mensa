09dc90be4315763158a4e66e6351c8868bcb335e
Tom de Grunt <tom@degrunt.nl>
2024-01-31 20:58:07 +0100

    Back to basic

--- lib/mensa/engine.rb
+++ lib/mensa/engine.rb
@@ -6,7 +6,6 @@ module Mensa
       Rails.application.reloader.to_prepare do
         ActiveSupport.on_load(:action_controller) do
           Satis.add_helper(:mensa, ::Mensa::TableContainer::Component)
-          Satis.add_component_helper :card, :with_mensa, ::Mensa::TableContainer::Component
         end
       end
     end
