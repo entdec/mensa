f1402c11d494255a3cd107418846a8de26fad415
Andre Meij <andre@itsmeij.com>
2024-07-22 14:04:06 +0000

    Fix pagination styling + don't auto submit on 'return/click'

--- app/components/mensa/search/component_controller.js
+++ app/components/mensa/search/component_controller.js
@@ -1,62 +1,63 @@
-import ApplicationController from 'mensa/controllers/application_controller'
+import ApplicationController from "mensa/controllers/application_controller";
 
-import { get } from '@rails/request.js'
+import { get } from "@rails/request.js";
 
 export default class SearchComponentController extends ApplicationController {
-  static targets = [
-    'resetSearchButton',
-    'searchInput'
-  ]
-  static outlets = [
-    "mensa-table"
-  ]
-  connect () {
-    super.connect()
-    this.monitorSearch()
+  static targets = ["resetSearchButton", "searchInput"];
+  static outlets = ["mensa-table"];
+  connect() {
+    super.connect();
+    this.monitorSearch();
   }
 
-  monitorSearch (event) {
+  monitorSearch(event) {
+    event && event.preventDefault();
+
     if (this.searchInputTarget.value.length >= 1) {
-      this.resetSearchButtonTarget.classList.remove('hidden')
+      this.resetSearchButtonTarget.classList.remove("hidden");
       this.searchInputTarget.focus();
     } else {
-      this.resetSearchButtonTarget.classList.add('hidden')
+      this.resetSearchButtonTarget.classList.add("hidden");
     }
   }
 
-  resetSearch (event) {
-    this.searchInputTarget.value = ''
-    this.searchInputTarget.focus()
-    this.resetSearchButtonTarget.classList.add('hidden')
+  resetSearch(event) {
+    event.preventDefault();
 
-    let turboFrame = this.element.closest('turbo-frame')
-    let url = this.ourUrl
-    url.searchParams.delete('query')
+    this.searchInputTarget.value = "";
+    this.searchInputTarget.focus();
+    this.resetSearchButtonTarget.classList.add("hidden");
+
+    let turboFrame = this.element.closest("turbo-frame");
+    let url = this.ourUrl;
+    url.searchParams.delete("query");
 
     get(url, {
-      responseKind: 'turbo-stream'
-    })
+      responseKind: "turbo-stream",
+    });
   }
 
-  search (event) {
+  search(event) {
+    event.preventDefault();
+
     if (this.query.length < 3) {
-      return
+      return;
     }
 
     // FIXME: This doesn't prevent searching twice on enter, the turbo-frame URL doesn't change
-    let url = this.ourUrl
-    if(url.searchParams.get('query') === this.query) {
-      return
+    let url = this.ourUrl;
+    if (url.searchParams.get("query") === this.query) {
+      return;
     }
 
-    url.searchParams.append('query', this.query)
+    url.searchParams.append("query", this.query);
 
     get(url, {
-      responseKind: 'turbo-stream'
-    })
+      responseKind: "turbo-stream",
+    });
   }
 
   get query() {
-    return this.searchInputTarget.value
+    return this.searchInputTarget.value;
   }
 }
--- app/components/mensa/table/component_controller.js
+++ app/components/mensa/table/component_controller.js
@@ -1,72 +1,78 @@
-import ApplicationController from 'mensa/controllers/application_controller'
-import { get } from '@rails/request.js'
+import ApplicationController from "mensa/controllers/application_controller";
+import { get } from "@rails/request.js";
 
 export default class TableComponentController extends ApplicationController {
   static targets = [
-    'controlBar',           // Bar with buttons
-    'condenseExpandIcon',   // Icon
-    'filters',              // Tabs or list of filters
-    'views',                // Tabs or list of views
-    'viewButtons',          // Cancel and save buttons for views
-    'search',               // Search bar
-    'view',                 // View contains table element
-    'turboFrame'            // The turbo-frame
-  ]
+    "controlBar", // Bar with buttons
+    "condenseExpandIcon", // Icon
+    "filters", // Tabs or list of filters
+    "views", // Tabs or list of views
+    "viewButtons", // Cancel and save buttons for views
+    "search", // Search bar
+    "view", // View contains table element
+    "turboFrame", // The turbo-frame
+  ];
   static values = {
-    supportsViews: Boolean
-  }
+    supportsViews: Boolean,
+  };
 
-  connect () {
-    super.connect()
+  connect() {
+    super.connect();
   }
 
   openFiltersAndSearch(event) {
-    if(this.supportsViewsValue) {
-      this.viewButtonsTarget.classList.remove('hidden')
-      this.searchTarget.classList.remove('hidden')
-      this.viewsTarget.classList.add('hidden')
-      this.filtersTarget.classList.remove('hidden')
+    event.preventDefault();
+
+    if (this.supportsViewsValue) {
+      this.viewButtonsTarget.classList.remove("hidden");
+      this.searchTarget.classList.remove("hidden");
+      this.viewsTarget.classList.add("hidden");
+      this.filtersTarget.classList.remove("hidden");
     } else {
-      this.controlBarTarget.classList.add('hidden')
-      this.viewButtonsTarget.classList.remove('hidden')
-      this.filtersTarget.classList.remove('hidden')
+      this.controlBarTarget.classList.add("hidden");
+      this.viewButtonsTarget.classList.remove("hidden");
+      this.filtersTarget.classList.remove("hidden");
     }
   }
 
   cancelFiltersAndSearch(event) {
-    if(this.supportsViewsValue) {
-      this.searchTarget.classList.add('hidden')
-      this.viewButtonsTarget.classList.add('hidden')
-      this.filtersTarget.classList.add('hidden')
-      this.viewsTarget.classList.remove('hidden')
+    event.preventDefault();
+
+    if (this.supportsViewsValue) {
+      this.searchTarget.classList.add("hidden");
+      this.viewButtonsTarget.classList.add("hidden");
+      this.filtersTarget.classList.add("hidden");
+      this.viewsTarget.classList.remove("hidden");
     } else {
-      this.controlBarTarget.classList.remove('hidden')
-      this.viewButtonsTarget.classList.add('hidden')
-      this.filtersTarget.classList.add('hidden')
+      this.controlBarTarget.classList.remove("hidden");
+      this.viewButtonsTarget.classList.add("hidden");
+      this.filtersTarget.classList.add("hidden");
     }
   }
 
   saveFiltersAndSearch(event) {
-
+    event.preventDefault();
   }
 
-  condenseExpand (event) {
-    if (this.viewTarget.classList.contains('mensa-table__condensed')) {
-      this.viewTarget.classList.remove('mensa-table__condensed')
-      this.condenseExpandIconTarget.classList.add('fa-compress')
-      this.condenseExpandIconTarget.classList.remove('fa-expand')
+  condenseExpand(event) {
+    event.preventDefault();
+
+    if (this.viewTarget.classList.contains("mensa-table__condensed")) {
+      this.viewTarget.classList.remove("mensa-table__condensed");
+      this.condenseExpandIconTarget.classList.add("fa-compress");
+      this.condenseExpandIconTarget.classList.remove("fa-expand");
     } else {
-      this.viewTarget.classList.add('mensa-table__condensed')
-      this.condenseExpandIconTarget.classList.remove('fa-compress')
-      this.condenseExpandIconTarget.classList.add('fa-expand')
+      this.viewTarget.classList.add("mensa-table__condensed");
+      this.condenseExpandIconTarget.classList.remove("fa-compress");
+      this.condenseExpandIconTarget.classList.add("fa-expand");
     }
   }
 
   export(event) {
-    let url = this.ourUrl
-    url.pathname += ".xlsx"
-    get(url, {
-    }).then(() => {
-    })
+    event.preventDefault();
+
+    let url = this.ourUrl;
+    url.pathname += ".xlsx";
+    get(url, {}).then(() => {});
   }
 }
