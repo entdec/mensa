3b75c929f62f38625bba7bec2f628f8b623d5450
Andre Meij <andre@itsmeij.com>
2025-02-07 15:53:08 +0000

    Add devcontainer setup (step1)

--- test/dummy/config/puma.rb
+++ test/dummy/config/puma.rb
@@ -22,14 +22,17 @@ end
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Specifies the `environment` that Puma will run in.
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+ssl_bind "0.0.0.0", 3000, {
+  key: File.join(".ssl", "localhost-key.pem"),
+  cert: File.join(".ssl", "localhost.pem"),
+  verify_mode: "none"
+}
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
