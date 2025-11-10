db4dca40850b620afdcbdc5a55a674429c3595a3
Tom de Grunt <tom@degrunt.nl>
2025-11-10 19:06:47 +0000

    Bugfix in test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,6 +24,9 @@ class TestTable < Mensa::Base
     column(:name) do
       attribute 'CONCAT(first_name, last_name)'
     end
+    column(:role) do
+      filter
+    end
 
     link { |test| root_path }
 
